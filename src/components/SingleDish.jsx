1589f3dd0af51387a25006b09c5c2e9865ffe107
massiliatazit <massiliatazit@users.noreply.github.com>
2020-11-25 15:48:37 +0100

    check if dish exist by adding if/else st and adding alert in case not found

--- src/components/SingleDish.jsx
+++ src/components/SingleDish.jsx
@@ -1,32 +1,37 @@
 import React from "react";
-import { Row, Col, Image, Badge } from "react-bootstrap";
+import { Row, Col, Image, Badge, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const SingleDish = (props) => (
-  <>
-    <Row>
-      <Col md={8} className="ml-auto mr-auto">
+const SingleDish = (props) => {
+  if ({ dish }) {
+    return (
+      <>
         <Row>
-          <Col md={4}>
-            <Link to={"/details/" + props.dish.id}>
-              <Image
-                src={props.dish.image}
-                className="my-2 w-100 h-auto"
-              />
-            </Link>
-          </Col>
-          <Col md={8} className="mt-auto mb-auto">
-            <h4>
-              {props.dish.name}
-              <Badge variant="warning" className="ml-2">{props.dish.price}</Badge>
-              <Badge variant="danger">{props.dish.label}</Badge>
-            </h4>
-            {props.dish.description}
+          <Col md={8} className="ml-auto mr-auto">
+            <Row>
+              <Col md={4}>
+                <Link to={"/details/" + props.dish.id}>
+                  <Image src={props.dish.image} className="my-2 w-100 h-auto" />
+                </Link>
+              </Col>
+              <Col md={8} className="mt-auto mb-auto">
+                <h4>
+                  {props.dish.name}
+                  <Badge variant="warning" className="ml-2">
+                    {props.dish.price}
+                  </Badge>
+                  <Badge variant="danger">{props.dish.label}</Badge>
+                </h4>
+                {props.dish.description}
+              </Col>
+            </Row>
           </Col>
         </Row>
-      </Col>
-    </Row>
-  </>
-)
+      </>
+    );
+  } else {
+    return <Alert variant="danger"> Something is wrong! Dish not found.</Alert>;
+  }
+};
 
 export default SingleDish;
