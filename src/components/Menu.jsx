b2df072bca505ef7ae22bc96231ce308fe6eaf1b
massiliatazit <massiliatazit@users.noreply.github.com>
2020-11-25 15:56:58 +0100

    short-circuit operator to check if array is empty then display alert work in progress

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import dishes from "../data/menu.json";
 import SingleDish from "./SingleDish.jsx";
-import { Container } from "react-bootstrap";
+import { Alert, Container } from "react-bootstrap";
 
 const Menu = () => (
   <Container>
     {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+    {dishes.length === 0 && (
+      <Alert variant="warning">work in progress.....</Alert>
+    )}
   </Container>
 );
 export default Menu;
