4a70ace4ef346f97a5d7451279e13e9bab904446
massiliatazit <massiliatazit@users.noreply.github.com>
2020-11-25 16:43:32 +0100

    label added when the user is in details page of Cacio & Pepe only

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Badge } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 
 class NavBar extends React.Component {
@@ -8,44 +8,87 @@ class NavBar extends React.Component {
     console.log("Just started to render");
   }
   render() {
-    console.log(this.props);
-    return (
-      <div>
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-          <Link to="/">
-            <Navbar.Brand>{this.props.title} - Strive For Food</Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-              <Link to="/menu">
-                <div
-                  className={
-                    this.props.location.pathname === "/menu"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Menu
-                </div>
-              </Link>
-              <Link to="/reservation">
-                <div
-                  className={
-                    this.props.location.pathname === "/reservation"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Reservation
-                </div>
-              </Link>
-              <Nav.Link>Our Location</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
+    console.log("I am Navbar props path", this.props.location.pathname);
+    if (this.props.location.pathname === "/details/2") {
+      return (
+        <div>
+          <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+            <Link to="/">
+              <Navbar.Brand>{this.props.title} - Strive For Food</Navbar.Brand>
+              <Badge variant="info">My favorite!</Badge>
+            </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto">
+                <Link to="/menu">
+                  <div
+                    className={
+                      this.props.location.pathname === "/menu"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                  >
+                    Menu
+                  </div>
+                </Link>
+
+                <Link to="/reservation">
+                  <div
+                    className={
+                      this.props.location.pathname === "/reservation"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                  >
+                    Reservation
+                  </div>
+                </Link>
+                <Nav.Link>Our Location</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+            <Link to="/">
+              <Navbar.Brand>{this.props.title} - Strive For Food</Navbar.Brand>
+            </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto">
+                <Link to="/menu">
+                  <div
+                    className={
+                      this.props.location.pathname === "/menu"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                  >
+                    Menu
+                  </div>
+                </Link>
+
+                <Link to="/reservation">
+                  <div
+                    className={
+                      this.props.location.pathname === "/reservation"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                  >
+                    Reservation
+                  </div>
+                </Link>
+                <Nav.Link>Our Location</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      );
+    }
   }
 }
 
